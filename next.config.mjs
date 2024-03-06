5248496a033fcb0a62d2ac5b3002848a35d8bf42
Pannarai Sihongthong <pannarai.st42@gmail.com>
2024-03-07 00:56:08 +0700

    config headers

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://shiny-day.vercel.app", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
